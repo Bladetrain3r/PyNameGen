43fd75918d5eb79e30a4871debe17c9f72977ce6
Blade <bladetrain3r@gmail.com>
2021-03-04 16:20:38 +0200

    First Commit

---
+++ ASCIIgen.py
@@ -0,0 +1,43 @@
+import random
+from os import path
+
+# Randomly Generate a Name
+randomwords = []
+
+if path.exists("words.txt"):    
+    while len(randomwords) <= 200:
+        pickword = random.choice(open("words.txt").readlines())
+        # print(pickword)
+        randomwords.append(pickword.strip('\n'))
+else:
+    randomwords.append(str(input("Custom Name? ")))
+
+def prefixgen(prefixlength, basename):
+    alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+    shipname = str(basename)
+    preflength = abs(int(prefixlength))
+
+    prefix = []
+    for item in range(preflength):
+        prefix.append(alphabet.split()[random.randrange(0, 25)])
+    return str("%s %s" % ("".join(prefix).upper(), shipname))
+
+def namegen(namelength, wordlist):
+    namelist = []
+    if len(wordlist) > 1:
+        while len(namelist) < namelength:
+            namelist.append(wordlist[random.randrange(0, len(wordlist))])
+        return " ".join(namelist)
+    else:
+        return wordlist[0]
+
+#Change number of words in the name and number of letters in the prefix here.
+hasprefix = input("Prefix? Y/N").lower()
+finalname = namegen(2, randomwords)
+if hasprefix == "y":
+    print(prefixgen(3, finalname))
+else:
+    print(finalname)
+
+
+
